04d346c7cea77f9a600c2c61e4a8c3c94a79bfca
henrique <henrique@henrique-H87M-D3H>
2020-08-17 08:29:08 -0300

    #17 cria filtro investidor águia conservador

--- views/acao-bolsa/_grid.php
+++ views/acao-bolsa/_grid.php
@@ -3,60 +3,62 @@
 use app\models\AcaoBolsaSearch;
 use kartik\grid\GridView;
 use yii\data\ActiveDataProvider;
-
 use yii\web\View;
+
 //use kartik\icons\Icon;
-
-
- // Maps the Elusive icon font framework
+// Maps the Elusive icon font framework
 //Icon::map($this);  
 /* @var $this View */
 /* @var $searchModel AcaoBolsaSearch */
 /* @var $dataProvider ActiveDataProvider */
-
 ?>
- 
 
 
 
-    <?=
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-           'data',
-            'patrimonio_liquido',
-            'receita_liquida',
-            'ebitda',
-            'da',
-            'ebit',
-            'margem_ebit',
-            'resultado_financeiro',
-            'imposto',
-            'lucro_liquido',
-            'margem_liquida',
-            'roe',
-            'caixa',
-            'divida_bruta',
-            'divida_liquida',
-            'divida_bruta_patrimonio',
-            'divida_liquida_ebitda',
-            'fco',
-            'capex',
-            'fcf',
-            'fcl',
-            'fcl_capex',
-            'proventos',
-            'payout',
-            'pdd',
-            'pdd_lucro_liquido',
-            'indice_basileia',
+
+<?=
+
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'options' => ['style' => 'font-size:10px;'],
+    'columns' => [
+        [
+            'attribute' =>'data',
         ],
-        
-         'toolbar'=>null,
-        'summary'=>false,
-         'floatHeader' => false,
-    ]);
-    ?>
+        [
+        'attribute' => 'patrimonio_liquido',
+            ],
+        'receita_liquida',
+        'ebitda',
+        'da',
+        'ebit',
+        'resultado_financeiro',
+        'imposto',
+        'margem_ebit',
+        'lucro_liquido',
+        'margem_liquida',
+        'roe',
+        'caixa',
+        'divida_bruta',
+        'divida_liquida',
+        'divida_bruta_patrimonio',
+        'divida_liquida_ebitda',
+        'fco',
+        'capex',
+        'fcf',
+        'fcl',
+        'fcl_capex',
+        'proventos',
+        'payout',
+        'pdd',
+        'pdd_lucro_liquido',
+        'indice_basileia',
+    ],
+    'toolbar' => null,
+    'summary' => false,
+    'floatHeader' => false,
+]);
+?>
 
 
 
