0c45d2fc172cc4404764be0528673b727136b2a3
henrique <henrique@henrique-H87M-D3H>
2020-12-16 10:38:31 -0300

    cria pasta de imagens

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -30,7 +30,7 @@ $this->registerJs(
 ?>
 
 <header class="main-header">
-<?= Html::a('<span class="logo-mini">APP</span><span><img src="/assets/img/logo2-1.png" /></span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+<?= Html::a('<span class="logo-mini">APP</span><span><img src="/img/logo5.png" /></span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
