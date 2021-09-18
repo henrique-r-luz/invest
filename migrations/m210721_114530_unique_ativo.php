631c67a3dff40cd43f8ee3f46887149a9e7fc061
henrique <henrique@henrique-H87M-D3H>
2021-09-18 12:02:33 -0300

    #22 corrige dashboard.

---
+++ migrations/m210721_114530_unique_ativo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\migrations;
+
+use yii\db\Migration;
+use \app\models\financas\Ativo;
+
+/**
+ * Class m190609_143226_inicio
+ */
+class m210721_114530_unique_ativo extends Migration {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp() {
+           $this->execute("drop index ativo_codigo;");
+           $this->execute("CREATE UNIQUE INDEX ativo_codigo_investidor ON  ativo (codigo,investidor_id);");
+       
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown() {
+        return true;
+    }
+
+}
--- models/dashboard/DashBoardSearch.php
+++ models/dashboard/DashBoardSearch.php
@@ -44,15 +44,18 @@ class DashBoardSearch {
        
 
 
-        $valorTotal = Ativo::find()->select(['sum(valor_bruto) as valor_total', 'sum(valor_compra) as valor_compra']);
+        $valorTotal = Ativo::find()->select(['sum(valor_bruto) as valor_total', 'sum(valor_compra) as valor_compra'])
+                                   ->where(['ativo' => true]);
+                       
 
         $ativos = Ativo::find()
-                ->select(['ativo.codigo', 'valor_bruto', 'ativo.categoria', 'valor_categoria', 'ativo.pais', 'valor_pais', 'ativo.tipo', 'valor_tipo', 'valor_acao_pais'])
+                ->select(['ativo.codigo', 'sum(valor_bruto) as valor_bruto', 'ativo.categoria', 'valor_categoria', 'ativo.pais', 'valor_pais', 'ativo.tipo', 'valor_tipo', 'valor_acao_pais'])
                 ->leftJoin(['ativo_categoria' => $categorio], 'ativo.categoria = ativo_categoria.categoria')
                 ->leftJoin(['ativo_tipo' => $tipo], 'ativo.tipo = ativo_tipo.tipo')
                 ->leftJoin(['ativo_pais' => $pais], 'ativo.pais = ativo_pais.pais')
                 ->leftJoin(['ativo_acao_pais' => $acaoPais], 'ativo_acao_pais.pais = ativo.pais')
-                ->where(['ativo' => true]);
+                ->where(['ativo' => true])
+                ->groupBy(['ativo.codigo', 'ativo.categoria', 'valor_categoria', 'ativo.pais', 'valor_pais', 'ativo.tipo', 'valor_tipo', 'valor_acao_pais']);
 
 
         $dados = (new \yii\db\Query())
