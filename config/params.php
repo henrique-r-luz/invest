2f8d59a92b08b12e417b5cf3aa7b6abf03413cfc
henrique <henrique@henrique-H87M-D3H>
2021-10-16 12:01:06 -0300

    #22  ajusta messagem de erro para atualização easyInvest.

--- config/params.php
+++ config/params.php
@@ -5,7 +5,7 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'serverNode'=>'http://192.168.200.10:3001',
-    'bot'=>'/var/www/invest/bot/dados',
+    'bot'=>'/var/www/dados',
     'icon-framework' => \kartik\icons\Icon::FAS,  // Font Awesome Icon framework
      
 ];
