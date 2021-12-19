04f8711ee0e7599bfdc134fa87dccb8c27821be9
henrique <henrique@henrique-H87M-D3H>
2021-12-19 13:42:50 -0300

    ajuste param

--- config/params.php
+++ config/params.php
@@ -7,7 +7,6 @@ return [
     'serverNode'=>'http://192.168.200.10:3001',
     'bot'=>'/var/www/dados',
     'ip_docker'=>'172.19.0.1',
-
     'back_up'=>'/var/www/back',
     'container_web'=>'invest_dev_invest_db_prod_1',
     'codigo_cdb_inter'=>32,
