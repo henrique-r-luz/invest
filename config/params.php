c2828cd2949c51b23ac4026b14db2665a46ad987
henrique <henrique@henrique-H87M-D3H>
2021-12-19 13:26:14 -0300

    teste ajuste

--- config/params.php
+++ config/params.php
@@ -7,11 +7,11 @@ return [
     'serverNode'=>'http://192.168.200.10:3001',
     'bot'=>'/var/www/dados',
     'ip_docker'=>'172.19.0.1',
-    
+
     'back_up'=>'/var/www/back',
     'container_web'=>'invest_dev_invest_db_prod_1',
     'codigo_cdb_inter'=>32,
-    'acesso'=>'acesso',
+    'acesso'=>'dandelo',
     'icon-framework' => \kartik\icons\Icon::FAS,  // Font Awesome Icon framework
      
 ];
