87ba7b3fd1de5c9d68803302de362672bf3f6ac3
henrique <henrique@henrique-H87M-D3H>
2022-01-19 19:01:48 -0300

    Atualiza o sistema para Bootstrap 4 #36

--- views/hail812/yii2-adminlte3/src/views/layouts/sidebar.php
+++ views/hail812/yii2-adminlte3/src/views/layouts/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="/img/logo2.png" alt="Invest Logo" class="brand-image" >
+        <span class="brand-text font-weight-light">INVEST</span>
     </a>
 
     <!-- Sidebar -->
