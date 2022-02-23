f216f22f6039a0e7d41c532a1622fe2d793cf099
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2022-02-23 20:30:11 -0300

    #52  ajusta arquivos sincronização

--- models/financas/service/sincroniza/SincronizaFactory.php
+++ models/financas/service/sincroniza/SincronizaFactory.php
@@ -11,11 +11,8 @@ namespace app\models\financas\service\sincroniza;
 use app\models\financas\service\sincroniza\CotacoesAcao;
 use app\models\financas\service\sincroniza\CotacaoCambio;
 use app\models\financas\service\operacoesImport\OperacaoNu;
-use app\models\financas\service\sincroniza\CotacoesAcaoApi;
-use app\models\financas\service\sincroniza\CotacaoCambioApi;
 use app\models\financas\service\operacoesImport\OperacaoInter;
-use app\models\financas\service\sincroniza\InseriEmpresasBolsa;
-use app\models\financas\service\operacoesImport\OperacaoInterSicroniza;
+
 
 /**
  * Description of Sincronizafactory
@@ -32,16 +29,10 @@ class SincronizaFactory
         switch ($tipo) {
             case 'cambio':
                 return self::getCambio();
-            case 'cambioApi':
-                return self::getCambioApi();
             case 'acao':
                 return self::getAcao();
-            case 'acaoApi':
-                return self::getAcaoApi();
             case 'easy':
                 return self::getEasy();
-            case 'empresa':
-                return self::getEmpresa();
             case 'banco_inter':
                 return self::getBancoInter();
         }
@@ -52,19 +43,10 @@ class SincronizaFactory
         return new CotacoesAcao();
     }
 
-    private static function getAcaoApi()
-    {
-        return new CotacoesAcaoApi();
-    }
-
+   
     private static function getCambio()
     {
         return new CotacaoCambio();
-    }
-
-    private static function getCambioApi()
-    {
-        return new CotacaoCambioApi();
     }
 
 
@@ -73,10 +55,6 @@ class SincronizaFactory
         return new OperacaoNu(null);
     }
 
-    private static function getEmpresa()
-    {
-        return new InseriEmpresasBolsa();
-    }
 
     private static function getBancoInter()
     {
