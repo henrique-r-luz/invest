c4374ef75f8742ed5ed1527bac7a2496cc0e78e5
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2022-03-28 18:54:45 -0300

    ajuste data operações import

--- models/financas/OperacoesImportSearch.php
+++ models/financas/OperacoesImportSearch.php
@@ -80,8 +80,6 @@ class OperacoesImportSearch extends OperacoesImport
         $this->load($params);
 
         if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
             return $dataProvider;
         }
 
