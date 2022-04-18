89f5f48c0f09f340b3d127988f8bc96211ce75f0
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2022-04-17 21:03:12 -0300

    config debug

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
-phpinfo();
 // comment out the following two lines when deployed to production
-/*defined('YII_DEBUG') or define('YII_DEBUG', true);
+//phpinfo();
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 
@@ -10,4 +10,4 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
-(new yii\web\Application($config))->run();*/
+(new yii\web\Application($config))->run();
