02b727f908694b2036b0d827f29cc14e7c119901
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2023-08-06 10:20:46 -0300

    #84 Melhora importação dados b3

--- models/financas/service/operacoesImport/OperacaoProventos.php
+++ models/financas/service/operacoesImport/OperacaoProventos.php
@@ -4,7 +4,6 @@ namespace app\models\financas\service\operacoesImport;
 
 use Yii;
 use app\lib\CajuiHelper;
-use app\models\financas\Operacao;
 use app\models\financas\Proventos;
 use app\models\financas\ItensAtivo;
 use app\lib\helpers\InvestException;
@@ -15,9 +14,11 @@ use app\lib\config\atualizaAtivos\ComponenteOperacoes;
 
 class OperacaoProventos extends OperacoesImportAbstract
 {
+    private $proventosInseridos = [];
 
     protected function getDados()
     {
+        $this->proventosInseridos = [];
         $filePath = Yii::getAlias('@' . OperacoesImport::DIR) . '/' . $this->operacoesImport->hash_nome . '.' . $this->operacoesImport->extensao;
         if (!file_exists($filePath)) {
             throw new InvestException("O arquivo enviado não foi salvo no servidor. ");
@@ -29,11 +30,43 @@ class OperacaoProventos extends OperacoesImportAbstract
         unset($this->arquivo[0]);
     }
 
+    private function removeLinhasVazias($linha)
+    {
+        if ($linha[0] == null) {
+            return true;
+        }
+        return false;
+    }
+    private function uniaoDadosRepetidos($item)
+    {
+        $proventos = array_filter($this->proventosInseridos, function ($provento) use ($item) {
+            return (($provento->itens_ativos_id == $item->itens_ativos_id)
+                && $provento->valor == $item->valor
+                && $provento->data == $item->data);
+        });
+        if (!empty($proventos)) {
+            $objProvento = array_values($proventos);
+            $provento  = $objProvento[0];
+            $provento->valor += $provento->valor;
+            if (!$provento->save()) {
+                $erro = CajuiHelper::processaErros($provento->getErrors());
+                throw new InvestException($erro);
+            }
+            return true;
+        }
+        return false;
+    }
+
     public  function atualiza()
     {
+
         try {
             $transaction = Yii::$app->db->beginTransaction();
             foreach ($this->arquivo as $id => $linha) {
+                if ($this->removeLinhasVazias($linha)) {
+                    continue;
+                }
+
                 $ativoProvento = $linha[0];
                 $ativoProvento = str_replace(' ', '', $ativoProvento);
                 $ativoProvento = \explode('-', $ativoProvento)[0];
@@ -42,38 +75,44 @@ class OperacaoProventos extends OperacoesImportAbstract
                 $valorProvento = str_replace('R$', '', $valorProvento);
                 $valorProvento = str_replace('.', '', $valorProvento);
                 $valorProvento = str_replace(',', '.', $valorProvento);
-               
+
 
                 $data  = $linha[1];
-                
+
                 list($d, $m, $y) = explode('/', $data);
-                $data = $y . '-' . $m . '-' . ($d) ;
+                $data = $y . '-' . $m . '-' . ($d);
 
                 $movimentacao = $linha[2];
 
                 $provento = new Proventos();
                 $provento->itens_ativos_id =  ItensAtivo::find()
-                    ->innerJoin('ativo','itens_ativo.ativo_id = ativo.id')
+                    ->innerJoin('ativo', 'itens_ativo.ativo_id = ativo.id')
                     ->where(['ativo.codigo' => $ativoProvento])
                     ->andWhere(['investidor_id' => $this->operacoesImport->investidor_id])
                     ->one()
                     ->id;
                 $provento->valor = floatval($valorProvento);
                 $provento->data =  $data;
-                $provento->movimentacao =ProventosMovimentacao::getId($movimentacao);
+                $provento->movimentacao = ProventosMovimentacao::getId($movimentacao);
+
+                if ($this->uniaoDadosRepetidos($provento)) {
+                    continue;
+                }
 
                 if (!$provento->save()) {
                     $erro = CajuiHelper::processaErros($provento->getErrors());
                     $transaction->rollBack();
                     throw new InvestException($erro);
                 }
+                $this->proventosInseridos[] = $provento;
 
                 $this->dadosJson['operacoes_id'][] = $provento->id;
             }
             $transaction->commit();
         } catch (\Exception $e) {
-
             throw new \Exception($e->getMessage());
+        } catch (InvestException $e) {
+            throw $e;
         }
     }
 
