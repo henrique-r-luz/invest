281e5764113445453cb6fc8dc504717b2647d67d
Henrique Rodrigues Luz <henrique_r_luz@yahoo.com.br>
2025-11-12 15:37:58 -0300

    corrige api_preço

--- config/api_preco.php
+++ config/api_preco.php
@@ -5,8 +5,8 @@ use app\lib\componentes\ApiPreco;
 return [
    'class' => ApiPreco::class,
    'apiBitcoin' => 'https://blockchain.info/ticker',
-   'apiUsa' => 'https://financialmodelingprep.com/api/v3/quote-short/',
-   'apiUsaKey' => '?apikey=<sua api key>',
+   'apiUsa' => 'https://financialmodelingprep.com/stable/profile?symbol=',
+   'apiUsaKey' => '&apikey=<sua api key>',
    'apiBr' => 'https://brapi.dev/api/quote/',
    'apiBrKey' => '?token=<seu token key>',
    'apiMoeda' => 'https://economia.awesomeapi.com.br/last/USD-BRL',
